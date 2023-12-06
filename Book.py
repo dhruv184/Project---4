15654b113e3b59fa53b97d162fbb7497740385d9
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-12-06 18:04:46 -0500

    Added code in Book.py

---
+++ Book.py
@@ -0,0 +1,11 @@
+class Book:
+
+    def __init__(self , bookID , bookName , Author):
+
+        self.id = bookID
+        self.name = bookName
+        self.author = Author
+
+    def __str__(self):
+
+        return f" ID : {self.id}\nName : {self.name}\nAuthor : {self.author}\n"    
